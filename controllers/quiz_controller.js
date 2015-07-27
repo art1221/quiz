a74100785b3f5cbb7eb51e65c7ef8169407ff792
art1221 <arturo.rojas@utpuebla.edu.mx>
2015-07-27 11:13:41 -0500

    Cambio de pregunta
    Capital de México

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,11 @@
 //GET /quizes/question
 exports.question = function(req,res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	res.render('quizes/question', {pregunta: 'Capital de Mexico'});
 };
 
 //GET /quizes/answer
 exports.answer = function(req,res) {
-	if(req.query.respuesta === 'Roma'){
+	if(req.query.respuesta === 'DF'){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	} else {
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
